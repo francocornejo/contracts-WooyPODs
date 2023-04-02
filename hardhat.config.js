18acdf66c37cf7b53faf24ec08c14e447cd89d59
FrancoCornejoAndreani <fcornejo@andreani.com>
2023-04-02 14:53:02 -0300

    contrato funcionando

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-ethers");
+
 module.exports = {
-  solidity: "0.8.18",
+  solidity: "0.8.9",
+  networks: {
+    hardhat: {},
+    mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.OWNER_PRIVATE_KEY],  
+      /**
+       * {
+        mnemonic: process.env.OWNER_PRIVATE_KEY,
+      },
+      **/
+    },
+  },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,27 +1,13 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+  const WooyPods = await hre.ethers.getContractFactory("WOOYPODs");
+  const wooyPods = await WooyPods.deploy();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  await wooyPods.deployed();
 
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log("Deployed to " + wooyPods.address)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
